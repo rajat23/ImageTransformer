c0a9a80886fadd7b39c7ce10549e6fbc56230952
tejaswini1 <tejubhosle123@gmail.com>
2015-02-06 11:50:58 +0530

    added alternative code for scaling image in ScaleServlet2 class

---
+++ src/main/java/com/servlet/ScaleServlet2.java
@@ -0,0 +1,35 @@
+package com.servlet;
+import com.utility.ImageResizer2;
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class ScaleServlet2 extends HttpServlet {
+
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String url=request.getParameter("url");
+        int width = Integer.parseInt(request.getParameter("width"));
+        int height = Integer.parseInt(request.getParameter("height"));
+        BufferedImage image = null;
+
+        try {
+            image = Resize.resizeImage(url, width,height);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        response.setContentType("image/jpeg");
+        ImageIO.write(image, "jpg", response.getOutputStream());
+    }
+
+    public void destroy() {
+    }
+
+}
