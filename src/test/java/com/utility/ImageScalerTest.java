34e4a697253ae98af72f004ac8b64d735995fc70
Rajat <rajatc@thoughtworks.com>
2015-02-10 16:04:11 +0530

    test for scaling image implemented

--- src/test/java/com/utility/ImageScalerTest.java
+++ src/test/java/com/utility/ImageScalerTest.java
@@ -1,11 +1,25 @@
 package com.utility;
 
-import org.junit.Assert;
+
 import org.junit.Test;
+import java.awt.image.BufferedImage;
+import static org.junit.Assert.assertEquals;
 
 public class ImageScalerTest {
     @Test
     public void testResizeImage() throws Exception {
+        //action
+        ImageScaler imageScaler=new ImageScaler();
+        int width=800;
+        int height=800;
+        String url="http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/4/11/1397210130748/Spring-Lamb.-Image-shot-2-011.jpg";
+        //call
+        BufferedImage image=imageScaler.resizeImage(url, width, height);
+        //test
+        assertEquals(image.getHeight(),height);
+        assertEquals(image.getWidth(),width);
+
+        
 
     }
 
