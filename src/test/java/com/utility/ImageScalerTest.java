d04ffe404ac9b59efdd92d25e67cfc919316e6fe
Rajat <rajatc@thoughtworks.com>
2015-02-10 16:11:59 +0530

    refactoring done

--- src/test/java/com/utility/ImageScalerTest.java
+++ src/test/java/com/utility/ImageScalerTest.java
@@ -9,13 +9,18 @@ public class ImageScalerTest {
     @Test
     public void testResizeImage() throws Exception {
         //action
-        ImageScaler imageScaler=new ImageScaler();
+
         int width=800;
         int height=800;
         String url="http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/4/11/1397210130748/Spring-Lamb.-Image-shot-2-011.jpg";
+
         //call
+
+        ImageScaler imageScaler=new ImageScaler();
         BufferedImage image=imageScaler.resizeImage(url, width, height);
+
         //test
+        
         assertEquals(image.getHeight(),height);
         assertEquals(image.getWidth(),width);
 
