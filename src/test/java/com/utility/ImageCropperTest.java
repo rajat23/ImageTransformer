cf95118c02cab77376a484e94734b121f2b2db42
Akshaysingh Yaduvanshi <akshaysinghyaduvanshi@Thoughtworkss-MacBook-Air.local>
2015-02-11 12:19:26 +0530

    changed test to send image instead of url and removed imageCompresser test

--- src/test/java/com/utility/ImageCropperTest.java
+++ src/test/java/com/utility/ImageCropperTest.java
@@ -1,7 +1,10 @@
 package com.utility;
 
+import com.helper.ImageReader;
 import org.junit.Test;
 import java.awt.image.BufferedImage;
+import java.net.URL;
+
 import static org.junit.Assert.*;
 public class ImageCropperTest {
 
@@ -9,11 +12,13 @@ public class ImageCropperTest {
     public void testGetCroppedImage() throws Exception {
 
         String url="http://www.joomlaworks.net/images/demos/galleries/abstract/7.jpg";
+        ImageReader imageReader=new ImageReader();
+        BufferedImage image=imageReader.readImage(new URL(url));
         int x=10,y=10;
         int width=100,height=100;
 
         ImageCropper imageCropper=new ImageCropper();
-        BufferedImage croppedImage=imageCropper.getCroppedImage(url,x,y,width,height);
+        BufferedImage croppedImage=imageCropper.getCroppedImage(image,x,y,width,height);
 
         assertEquals(width,croppedImage.getWidth());
         assertEquals(height,croppedImage.getHeight());
--- src/test/java/com/utility/ImageScalerTest.java
+++ src/test/java/com/utility/ImageScalerTest.java
@@ -1,28 +1,32 @@
 package com.utility;
 
 
+import com.helper.ImageReader;
 import org.junit.Test;
 import java.awt.image.BufferedImage;
+import java.net.URL;
+
 import static org.junit.Assert.assertEquals;
 
 public class ImageScalerTest {
     @Test
     public void testResizeImage() throws Exception {
         //action
-
+        String url="http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/4/11/1397210130748/Spring-Lamb.-Image-shot-2-011.jpg";
         int width=800;
         int height=800;
-        String url="http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/4/11/1397210130748/Spring-Lamb.-Image-shot-2-011.jpg";
+        ImageReader imageReader=new ImageReader();
+        BufferedImage image=imageReader.readImage(new URL(url));
 
         //call
 
         ImageScaler imageScaler=new ImageScaler();
-        BufferedImage image=imageScaler.resizeImage(url, width, height);
+        BufferedImage resizedImage=imageScaler.resizeImage(image, width, height);
 
         //test
         
-        assertEquals(image.getHeight(),height);
-        assertEquals(image.getWidth(),width);
+        assertEquals(resizedImage.getHeight(),height);
+        assertEquals(resizedImage.getWidth(),width);
 
         
 
