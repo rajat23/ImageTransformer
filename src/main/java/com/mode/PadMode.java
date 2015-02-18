e6ee14655f8ef804e0d15996ada15f9a7c0b8a3e
tejaswini1 <tejubhosle123@gmail.com>
2015-02-18 12:24:05 +0530

    modified PadMode class

---
+++ src/main/java/com/mode/PadMode.java
@@ -0,0 +1,13 @@
+package com.mode;
+
+
+import com.utility.ImageStructure;
+
+import java.awt.image.BufferedImage;
+
+public class PadMode implements Mode {
+    public ImageStructure getDimensions(BufferedImage image, ImageStructure imageStructure) {
+
+        return null ;
+    }
+}
