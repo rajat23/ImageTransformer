a3adf8d624c3ccdbc67f124c212b37e53789d7c1
tejaswini1 <tejubhosle123@gmail.com>
2015-02-18 13:20:51 +0530

    modified LimitMode class to return BufferedImage

--- src/main/java/com/mode/LimitMode.java
+++ src/main/java/com/mode/LimitMode.java
@@ -1,16 +1,38 @@
 package com.mode;
 
+import apple.awt.CImage;
+import com.utility.ImageScaler;
 import com.utility.RequestStructure;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class LimitMode implements Mode {
-    public RequestStructure getDimensions(BufferedImage image, RequestStructure requestStructure) {
+    public BufferedImage getScaledImage(RequestStructure requestStructure) throws IOException {
+        BufferedImage image = requestStructure.getImage();
         if (requestStructure.getHeight() > image.getHeight() || requestStructure.getWidth() > image.getWidth())
-            return new RequestStructure(0, 0, image.getWidth(), image.getHeight());
+            return image;
 
-        return new FitMode().getDimensions(image, requestStructure);
 
+        float originalAspectRatio = (float) image.getWidth() / (float) image.getHeight();
+        int height = requestStructure.getHeight();
+        int width = requestStructure.getWidth();
+        if (height == 0) {
+            height = (int) (width / originalAspectRatio);
+
+        } else if (width == 0) {
+            width = (int) (height * originalAspectRatio);
+
+        } else if ((width / height) != originalAspectRatio) {
+            if (height > width)
+                height = (int) (width / originalAspectRatio);
+            else
+                width = (int) (height * originalAspectRatio);
+        }
+
+
+        return new ImageScaler().resizeImage(image,width,height);
     }
 }
 
