e3d22cf51064765863538330c2b9b256c88fedcc
Rajat <rajatc@thoughtworks.com>
2015-02-18 13:19:46 +0530

    Fit mode class implemented

--- src/main/java/com/mode/LimitMode.java
+++ src/main/java/com/mode/LimitMode.java
@@ -5,7 +5,7 @@ import com.utility.RequestStructure;
 import java.awt.image.BufferedImage;
 
 public class LimitMode implements Mode {
-    public RequestStructure getDimensions(BufferedImage image, RequestStructure requestStructure) {
+    public RequestStructure getScaledImage(BufferedImage image, RequestStructure requestStructure) {
         if (requestStructure.getHeight() > image.getHeight() || requestStructure.getWidth() > image.getWidth())
             return new RequestStructure(0, 0, image.getWidth(), image.getHeight());
 
--- src/main/java/com/utility/ImageScaler.java
+++ src/main/java/com/utility/ImageScaler.java
@@ -11,11 +11,8 @@ import java.io.IOException;
 public class ImageScaler {
 
     public BufferedImage resizeImage(BufferedImage image, int width, int height) throws IOException {
-        ModeFactory modeFactory = new ModeFactory();
-        Mode fitMode = modeFactory.create("fit");
-        RequestStructure requestStructure = fitMode.getDimensions(image, new RequestStructure(0, 0, width, height));
 
-        BufferedImage resizedImage = new BufferedImage(requestStructure.getWidth(), requestStructure.getHeight(), image.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : image.getType());
+        BufferedImage resizedImage = new BufferedImage(width, height, image.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : image.getType());
         Graphics2D g = resizedImage.createGraphics();
         g.drawImage(image, 0, 0, width, height, null);
         g.dispose();
