b97b048501c2ae505e3d84408bb8943bc19d764d
Akshaysingh Yaduvanshi <akshaysyaduvanshi@gmail.com>
2015-02-19 17:19:39 +0530

    modified code ShrinkDimension to check aspectRatio

--- src/main/java/com/mode/strategies/ExpandDimension.java
+++ src/main/java/com/mode/strategies/ExpandDimension.java
@@ -1,13 +1,6 @@
 package com.mode.strategies;
 
-import com.helper.RequestStructure;
-
 import java.awt.*;
-import java.awt.image.BufferedImage;
-
-/**
- * Created by akshaysinghyaduvanshi on 2/19/15.
- */
 public class ExpandDimension implements Strategy {
     public Dimension doOperation(Dimension dimension,float aspectRatio) {
 
--- src/main/java/com/mode/strategies/ShrinkDimension.java
+++ src/main/java/com/mode/strategies/ShrinkDimension.java
@@ -1,24 +1,14 @@
 package com.mode.strategies;
 
-import com.helper.RequestStructure;
-
 import java.awt.*;
-import java.awt.image.BufferedImage;
-
-/**
- * Created by akshaysinghyaduvanshi on 2/19/15.
- */
 public class ShrinkDimension implements Strategy {
     public Dimension doOperation(Dimension dimension,float aspectRatio){
         int width = (int)dimension.getWidth();
         int height =(int)dimension.getHeight();
-
-        if ((width / height) != aspectRatio) {
-            if (height > width)
-                height = (int) Math.ceil(width / aspectRatio);
-            else
-                width = (int) Math.ceil(height * aspectRatio);
-        }
+        if (aspectRatio>1.0f)
+            height = (int) Math.ceil(width / aspectRatio);
+        else if(aspectRatio<1.0f)
+            width = (int) Math.ceil(height * aspectRatio);
         return new Dimension(width,height);
     }
 }
