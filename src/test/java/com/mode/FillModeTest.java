0c74cbff7dff1af08ea4ccba4ab8a385cebe02f5
Akshaysingh Yaduvanshi <akshaysyaduvanshi@gmail.com>
2015-02-20 12:40:18 +0530

    added test class FillModeTest

--- src/test/java/com/mode/FillModeTest.java
+++ src/test/java/com/mode/FillModeTest.java
@@ -11,26 +11,48 @@ import java.util.Map;
 import static org.junit.Assert.*;
 
 public class FillModeTest {
-
-   /* @Test
+    @Test
     public void testGetScaledImage() throws Exception {
         String name="PngImage.png";
         ImageReader imageReader=new ImageReader();
         BufferedImage image=imageReader.readImage(name);
-
-        RequestStructure requestStructure =new RequestStructure(image);
+        RequestStructure requestStructure =new RequestStructure();
+        requestStructure.setImage(image);
         Map<String,String[]> map=new HashMap<String, String[]>();
         map.put("height",new String[]{"300"});
         map.put("width",new String[]{"400"});
         map.put("orientation",new String[]{"north_west"});
-
         requestStructure.setParameters(map);
         ModeFactory modeFactory=new ModeFactory();
         Mode fillMode=modeFactory.create("fill");
 
         BufferedImage scaledImage=fillMode.getScaledImage(requestStructure);
 
+        assertEquals(scaledImage.getWidth(),400);
+        assertEquals(scaledImage.getHeight(),300);
 
+    }
 
-    }*/
+    @Test
+    public void testGetScaledImageForBigDim() throws Exception {
+        String name="PngImage.png";
+        ImageReader imageReader=new ImageReader();
+        BufferedImage image=imageReader.readImage(name);
+        RequestStructure requestStructure =new RequestStructure();
+        requestStructure.setImage(image);
+        Map<String,String[]> map=new HashMap<String, String[]>();
+        map.put("height",new String[]{"1000"});
+        map.put("width",new String[]{"4000"});
+        map.put("orientation",new String[]{"north_west"});
+        requestStructure.setParameters(map);
+        ModeFactory modeFactory=new ModeFactory();
+        Mode fillMode=modeFactory.create("fill");
+
+        BufferedImage scaledImage=fillMode.getScaledImage(requestStructure);
+
+        assertEquals(scaledImage.getWidth(),4000);
+        assertEquals(scaledImage.getHeight(),1000);
+
+    }
+
 }
