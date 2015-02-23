fd495b8305aad6c417e17d94b906f0e52c5ff136
Akshaysingh Yaduvanshi <akshaysyaduvanshi@gmail.com>
2015-02-23 15:26:08 +0530

    modified ShrinkDimension

--- src/main/java/com/mode/strategies/ShrinkDimension.java
+++ src/main/java/com/mode/strategies/ShrinkDimension.java
@@ -5,9 +5,9 @@ public class ShrinkDimension implements Strategy {
     public Dimension doOperation(Dimension dimension,float aspectRatio){
         int width = (int)dimension.getWidth();
         int height =(int)dimension.getHeight();
-        if (aspectRatio>1.0f)
+        if (aspectRatio>(float)width/(float)height)
             height =  Math.round(width / aspectRatio);
-        else if(aspectRatio<1.0f)
+        else if(aspectRatio<(float)width/(float)height)
             width =  Math.round(height * aspectRatio);
         return new Dimension(width,height);
     }
