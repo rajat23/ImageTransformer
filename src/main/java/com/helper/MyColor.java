57e534ef806374e2a35ce94b28235b8a815faba8
Akshaysingh Yaduvanshi <akshaysyaduvanshi@gmail.com>
2015-02-24 16:33:29 +0530

    added method getColorFromRGB in MyColor

--- src/main/java/com/helper/MyColor.java
+++ src/main/java/com/helper/MyColor.java
@@ -7,6 +7,9 @@ public class MyColor {
         if(name.startsWith("0x")) {
             return Color.decode(name);
         }
+        if(name.startsWith("r_")||name.startsWith("g_")||name.startsWith("b_")){
+            return getColorFormRGB(name);
+        }
         if (name.equals("black"))
             return Color.BLACK;
         if (name.equals("blue"))
@@ -33,5 +36,17 @@ public class MyColor {
             return Color.YELLOW;
         return Color.WHITE;
 
+    }
+    public static Color getColorFormRGB(String name){
+        int r=0,g=0,b=0;
+        for (String color:name.split(",")){
+            if(color.startsWith("r_"))
+                r=Integer.parseInt(color.substring(2));
+            else if(color.startsWith("g_"))
+                g=Integer.parseInt(color.substring(2));
+            else  if(color.startsWith("r_"))
+                b=Integer.parseInt(color.substring(2));
+        }
+        return new Color(r,g,b);
     }
 }
--- src/test/java/com/helper/MyColorTest.java
+++ src/test/java/com/helper/MyColorTest.java
@@ -8,16 +8,11 @@ import static org.junit.Assert.*;
 
 public class MyColorTest {
 
-    @Test
-    public void testToHex() throws Exception {
-        String color="#123456";
-        String returnedColor=MyColor.toHex(color);
-        assertEquals(returnedColor,"0x123456");
-    }
+
 
     @Test
     public void testGetColor() throws Exception {
-        String color="#123456";
+        String color="0x123456";
         Color returnedColor=MyColor.getColor(color);
         assertEquals(returnedColor,Color.decode("0x123456"));
 
