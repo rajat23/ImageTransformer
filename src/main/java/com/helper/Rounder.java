5209d9975f6cd4748103b0662fc63746c3c80878
Akshaysingh Yaduvanshi <akshaysyaduvanshi@gmail.com>
2015-03-02 12:28:55 +0530

    modified Rounder class

--- src/main/java/com/helper/Rounder.java
+++ src/main/java/com/helper/Rounder.java
@@ -5,20 +5,21 @@ import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
 
 public class Rounder {
-    public static  BufferedImage makeRoundedCorner(BufferedImage image, int cornerRadius) {
+    public static  BufferedImage makeRoundedCorner(BufferedImage originalImage, int cornerRadius) {
         if(cornerRadius==0)
-            return image;
-        int width = image.getWidth();
-        int height = image.getHeight();
-        BufferedImage outputImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D graphics = outputImage.createGraphics();
-        graphics.setComposite(AlphaComposite.Src);
-        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        graphics.setBackground(Color.WHITE);
-        graphics.fill(new RoundRectangle2D.Float(0, 0, width, height, cornerRadius, cornerRadius));
-        graphics.setComposite(AlphaComposite.SrcAtop);
-        graphics.drawImage(image, 0, 0, null);
-        graphics.dispose();
+            return originalImage;
+        int width=originalImage.getWidth();
+        int height=originalImage.getHeight();
+        BufferedImage outputImage=new BufferedImage(width,height,originalImage.getType());
+        Graphics2D g2=outputImage.createGraphics();
+        g2.setBackground(Color.WHITE);
+        g2.fillRect(0,0,width,height);
+        g2.setComposite(AlphaComposite.Src);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.fill(new RoundRectangle2D.Float(0, 0, width, height, cornerRadius, cornerRadius));
+        g2.setComposite(AlphaComposite.SrcAtop);
+        g2.drawImage(originalImage, 0, 0, null);
+        g2.dispose();
         return outputImage;
     }
 }
