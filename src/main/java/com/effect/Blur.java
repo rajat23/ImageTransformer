3037cbce9ea74c5f227f6a5e80e4b548a8f42213
Akshaysingh Yaduvanshi <akshaysyaduvanshi@gmail.com>
2015-03-03 13:47:13 +0530

    added blur effect not working need to change

--- src/main/java/com/effect/Blur.java
+++ src/main/java/com/effect/Blur.java
@@ -1,6 +1,8 @@
 package com.effect;
 
 
+import com.utility.ImageScaler;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -10,10 +12,28 @@ import java.awt.image.Kernel;
 import java.io.InputStream;
 
 public class Blur implements  Effect{
+    public BufferedImage getEffect(BufferedImage image) {
+        return null;
+    }
 
-    public BufferedImage getEffect(BufferedImage image){
-    
-        return  image;
+    public BufferedImage getEffect(BufferedImage image,int factor){
+        int size=factor+2;
+        BufferedImage output = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_BGR);
+        Graphics g = output.getGraphics();
+        g.drawImage(image, 455, 255, null);
+        int radius=0;
+        factor+=2;
+        for(;factor>2;factor--)
+            radius+=factor*factor;
+        float[] blurKernel = new float[radius];
+        for(int i=0;i<radius;i++){
+            blurKernel[i]=1.0f/(float)radius;
+        }
+        radius=(int)Math.sqrt(radius);
+        BufferedImageOp blur = new ConvolveOp(new Kernel(radius,radius, blurKernel));
+        output = blur.filter(image, new BufferedImage(image.getWidth(), image.getHeight(), image.getType()));
+        g.dispose();
+        return output;
     }
 
 }
