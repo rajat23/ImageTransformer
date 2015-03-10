9c20fb5ca043cb0916959a55e2276f63ab79c7b2
Rajat <rajatc@thoughtworks.com>
2015-03-10 12:16:22 +0530

    rotation of image for a given angle is implemented

--- src/main/java/com/rotate/AngledFlip.java
+++ src/main/java/com/rotate/AngledFlip.java
@@ -1,11 +1,25 @@
 package com.rotate;
 
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class AngledFlip implements Angle {
     public BufferedImage rotateImage(BufferedImage image, String angle) {
         if(angle.equals("0"))
             return image;
+
+        double rotationAngle=Double.parseDouble(angle);
+        int widht = image.getWidth();
+        int height = image.getHeight();
+        BufferedImage dimg =new BufferedImage(widht, height, image.getType());
+        Graphics2D g = dimg.createGraphics();
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, // Anti-alias!
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g.rotate(Math.toRadians(rotationAngle), widht/2, height/2);
+
+        g.drawImage(image, null, 0, 0);
+        return dimg;
     }
 }
