636133bd55c3981141a415f4cd2817328c59db0c
Rajat <rajatc@thoughtworks.com>
2015-03-10 12:21:42 +0530

    hflip problem resolved

--- src/main/java/com/rotate/Angle.java
+++ src/main/java/com/rotate/Angle.java
@@ -1,7 +1,12 @@
 package com.rotate;
 
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 
 public interface Angle {
     public BufferedImage rotateImage(BufferedImage image,String angle);
-}
+
+
+    }
+
--- src/main/java/com/rotate/hflip.java
+++ src/main/java/com/rotate/hflip.java
@@ -4,12 +4,13 @@ import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 
-public class Hflip implements Angle {
-    public BufferedImage rotateImage(BufferedImage image, String angle) {
-        AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
-        tx.translate(-image.getWidth(null), 0);
-        AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-        image = op.filter(image, null);
-        return image;
-    }
+public class Hflip implements Angle{
+    
+        public BufferedImage rotateImage(BufferedImage image, String angle) {
+            AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
+            tx.translate(-image.getWidth(null), 0);
+            AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+            image = op.filter(image, null);
+            return image;
+        }
 }
