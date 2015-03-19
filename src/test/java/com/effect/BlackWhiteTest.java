21c85bb4069c3d8ee985eabec3c6bb1e57f8843d
Akshaysingh <akshaysyaduvanshi@gmail.com>
2015-03-19 13:23:38 +0530

    add tests for BlackWhite effect

--- src/test/java/com/effect/BlackWhiteTest.java
+++ src/test/java/com/effect/BlackWhiteTest.java
@@ -1,6 +1,8 @@
 package com.effect;
 
 import com.helper.ImageReader;
+import com.rotate.Angle;
+import com.rotate.AngleFactory;
 import org.junit.Test;
 
 import java.awt.*;
@@ -13,24 +15,37 @@ public class BlackWhiteTest {
 
     @Test
     public void testGetEffect() throws Exception {
-        String name="sample.jpg";
-        ImageReader imageReader=new ImageReader();
-        BufferedImage image=imageReader.readImage(name);
+        BufferedImage image=new BufferedImage(2,2,BufferedImage.TYPE_3BYTE_BGR);
+        image.setRGB(0, 0, new Color(22, 22, 22).getRGB());
+        image.setRGB(1, 0, new Color(222, 222, 222).getRGB());
+        image.setRGB(0, 1, new Color(211, 211, 211).getRGB());
+        image.setRGB(1, 1, new Color(44, 44, 44).getRGB());
 
         Effect effect=new EffectFactory().create("blackwhite");
         image=effect.getEffect(image);
-        
-        int isBlackWhite=1;
-        int width=image.getWidth();
-        int height=image.getHeight();
-        for(int j=0;j<height;j++)
-            for (int i=0;i<width;i++){
-                Color color=new Color(image.getRGB(i,j));
-                int r=color.getRed();
-                if(r!=0&&r!=255){
-                    isBlackWhite=0;}
-            }
-        assertEquals(1,isBlackWhite);
+
+        assertEquals(new Color(image.getRGB(0,0)),new Color(0,0,0));
+        assertEquals(new Color(image.getRGB(1,0)),new Color(255,255,255));
+        assertEquals(new Color(image.getRGB(0,1)),new Color(255,255,255));
+        assertEquals(new Color(image.getRGB(1,1)),new Color(0,0,0));
+
+    }
+
+    @Test
+    public void testGetEffectForParameter() throws Exception {
+        BufferedImage image=new BufferedImage(2,2,BufferedImage.TYPE_3BYTE_BGR);
+        image.setRGB(0, 0, new Color(122, 122, 122).getRGB());
+        image.setRGB(1, 0, new Color(222, 222, 222).getRGB());
+        image.setRGB(0, 1, new Color(211, 211, 211).getRGB());
+        image.setRGB(1, 1, new Color(144, 144, 144).getRGB());
+
+        Effect effect=new EffectFactory().create("blackwhite_150");
+        image=effect.getEffect(image);
+
+        assertEquals(new Color(image.getRGB(0,0)),new Color(0,0,0));
+        assertEquals(new Color(image.getRGB(1,0)),new Color(255,255,255));
+        assertEquals(new Color(image.getRGB(0,1)),new Color(255,255,255));
+        assertEquals(new Color(image.getRGB(1,1)),new Color(0,0,0));
 
     }
 }
