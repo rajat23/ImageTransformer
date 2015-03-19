9a8f6b1a36b75c7a7ad0fce17e15fd57b99919db
Akshaysingh <akshaysyaduvanshi@gmail.com>
2015-03-19 11:06:23 +0530

    added test class HflipTest and test for Hflip

---
+++ src/test/java/com/rotate/HflipTest.java
@@ -0,0 +1,30 @@
+package com.rotate;
+
+import com.effect.Effect;
+import com.effect.EffectFactory;
+import org.junit.Test;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import static org.junit.Assert.*;
+
+public class HflipTest {
+
+    @Test
+    public void testRotateImage() throws Exception {
+        BufferedImage image=new BufferedImage(2,2,BufferedImage.TYPE_3BYTE_BGR);
+        image.setRGB(0, 0, new Color(1, 1, 1).getRGB());
+        image.setRGB(1, 0, new Color(2, 2, 2).getRGB());
+        image.setRGB(0, 1, new Color(3, 3, 3).getRGB());
+        image.setRGB(1, 1, new Color(4, 4, 4).getRGB());
+
+        Angle angle=new AngleFactory().create("hflip");
+        image=angle.rotateImage(image,"hflip");
+
+        assertEquals(new Color(image.getRGB(0,0)),new Color(2,2,2));
+        assertEquals(new Color(image.getRGB(1,0)),new Color(1,1,1));
+        assertEquals(new Color(image.getRGB(0,1)),new Color(4,4,4));
+        assertEquals(new Color(image.getRGB(1,1)),new Color(3,3,3));
+    }
+}
