3d0ab614c8c3e6a5723884a2dae374fa93c1bc00
Akshaysingh <akshaysyaduvanshi@gmail.com>
2015-03-23 16:22:31 +0530

    Add ExpectedImageTest class and tests

---
+++ src/test/java/helper/ExpectedImageTest.java
@@ -0,0 +1,17 @@
+package helper;
+
+import org.junit.Test;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class ExpectedImageTest {
+    @Test
+    public void hasExpectedColorTest(){
+        BufferedImage image=new SampleImage().get10by10(100);
+        Color expectedColor=new Color(100,100,100);
+
+        ExpectedImage expectedImage=new ExpectedImage();
+        assertTrue(expectedImage.hasExpectedColor(image,expectedColor));
+    }
+}
