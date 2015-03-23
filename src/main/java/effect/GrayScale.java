ece10b2c33ce6d1315838e11fc2ac952b82c3739
Rajat <rajatc@thoughtworks.com>
2015-03-23 12:15:17 +0530

    GrayScale class refactored

--- src/main/java/effect/GrayScale.java
+++ src/main/java/effect/GrayScale.java
@@ -10,16 +10,16 @@ public class GrayScale implements Effectible {
         int width = sourceImage.getWidth();
         int height = sourceImage.getHeight();
 
-        for (int i = 0; i < height; i++) {
+        for (int heightCounter = 0; heightCounter < height; heightCounter++) {
 
-            for (int j = 0; j < width; j++) {
+            for (int widthCounter = 0; widthCounter < width; widthCounter++) {
 
-                Color color = new Color(sourceImage.getRGB(j, i));
+                Color color = new Color(sourceImage.getRGB(widthCounter, heightCounter));
                 int red = (int) (color.getRed() * 0.299);
                 int green = (int) (color.getGreen() * 0.587);
                 int blue = (int) (color.getBlue() * 0.114);
                 Color newColor = new Color(red + green + blue, red + green + blue, red + green + blue);
-                sourceImage.setRGB(j, i, newColor.getRGB());
+                sourceImage.setRGB(widthCounter, heightCounter, newColor.getRGB());
 
             }
         }
