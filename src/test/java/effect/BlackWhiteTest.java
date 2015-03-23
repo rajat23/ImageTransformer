b03d0421f24aadf9979fb0e1dad0633252c7ab05
Akshaysingh <akshaysyaduvanshi@gmail.com>
2015-03-23 16:46:26 +0530

    refactored BlackWhiteTest.java

--- src/test/java/effect/BlackWhiteTest.java
+++ src/test/java/effect/BlackWhiteTest.java
@@ -1,5 +1,6 @@
 package effect;
 
+import helper.ExpectedImage;
 import helper.SampleImage;
 import org.junit.After;
 import org.junit.Before;
@@ -11,39 +12,27 @@ import java.awt.image.BufferedImage;
 import static org.junit.Assert.*;
 
 public class BlackWhiteTest {
-    private BufferedImage image;
-
-    @Before
-    public void beforeEachTest(){
-        image=new SampleImage().get2by2();
-    }
 
     @Test
     public void testGetEffect() throws Exception {
-
+        BufferedImage image = new SampleImage().get2by2();
         Effectible effectible =new EffectFactory().create("blackwhite");
-        image= effectible.getEffect(image);
 
-        assertEquals(new Color(image.getRGB(0,0)),new Color(0,0,0));
-        assertEquals(new Color(image.getRGB(1,0)),new Color(0,0,0));
-        assertEquals(new Color(image.getRGB(0,1)),new Color(0,0,0));
-        assertEquals(new Color(image.getRGB(1,1)),new Color(0,0,0));
+        image= effectible.getEffect(image);
+        Color expectedColor = new Color(0,0,0);
+
+        assertTrue(new ExpectedImage().hasExpectedColor(image,expectedColor));
     }
 
     @Test
     public void testGetEffectForParameter() throws Exception {
+        BufferedImage image = new SampleImage().get3by2();
+        Effectible effectible =new EffectFactory().create("blackwhite_0");
 
-        Effectible effectible =new EffectFactory().create("blackwhite_3");
         image= effectible.getEffect(image);
+        Color expectedColor = new Color(255,255,255);
 
-        assertEquals(new Color(image.getRGB(0,0)),new Color(0,0,0));
-        assertEquals(new Color(image.getRGB(1,0)),new Color(0,0,0));
-        assertEquals(new Color(image.getRGB(0,1)),new Color(255,255,255));
-        assertEquals(new Color(image.getRGB(1,1)),new Color(255,255,255));
-
-    }
-    @After
-    public void afterEachTest(){
+        assertTrue(new ExpectedImage().hasExpectedColor(image,expectedColor));
 
     }
 }
