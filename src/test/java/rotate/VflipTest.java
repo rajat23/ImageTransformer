c9fd73e6e4b2925ce19646e7e2065f411eee86cd
tejaswini1 <tejubhosle123@gmail.com>
2015-03-23 11:43:26 +0530

    added test for vflip class

--- src/test/java/rotate/VflipTest.java
+++ src/test/java/rotate/VflipTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.*;
 public class VflipTest {
 
     @Test
-    public void testRotateImage() throws Exception {
+    public void testRotateSquareImage() throws Exception {
 
         BufferedImage image=new BufferedImage(2,2,BufferedImage.TYPE_3BYTE_BGR);
         image.setRGB(0, 0, new Color(1, 1, 1).getRGB());
@@ -25,5 +25,31 @@ public class VflipTest {
         assertEquals(new Color(image.getRGB(1,0)),new Color(4,4,4));
         assertEquals(new Color(image.getRGB(0,1)),new Color(1,1,1));
         assertEquals(new Color(image.getRGB(1,1)),new Color(2,2,2));
+    }
+
+
+
+
+
+    @Test
+    public void testRotateRectangleImage() throws Exception {
+
+        BufferedImage image=new BufferedImage(3,2,BufferedImage.TYPE_3BYTE_BGR);
+        image.setRGB(0, 0, new Color(1, 1, 1).getRGB());
+        image.setRGB(1, 0, new Color(2, 2, 2).getRGB());
+        image.setRGB(2, 0, new Color(6, 6, 6).getRGB());
+        image.setRGB(0, 1, new Color(3, 3, 3).getRGB());
+        image.setRGB(1, 1, new Color(4, 4, 4).getRGB());
+        image.setRGB(2, 1, new Color(5, 5, 5).getRGB());
+
+        Rotable rotable =new RotatorFactory().create("vflip");
+        image= rotable.rotateImage(image);
+
+        assertEquals(new Color(image.getRGB(0,0)),new Color(3,3,3));
+        assertEquals(new Color(image.getRGB(1,0)),new Color(4,4,4));
+        assertEquals(new Color(image.getRGB(2,0)),new Color(5,5,5));
+        assertEquals(new Color(image.getRGB(0,1)),new Color(1,1,1));
+        assertEquals(new Color(image.getRGB(1,1)),new Color(2,2,2));
+        assertEquals(new Color(image.getRGB(2,1)),new Color(6,6,6));
     }
 }
