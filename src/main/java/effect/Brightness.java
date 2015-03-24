d293022b4986ccbc8a9462e1becb14db192e22e5
Akshaysingh <akshaysyaduvanshi@gmail.com>
2015-03-24 15:01:10 +0530

    Extract redunt code in method checkBoundaryValue

--- src/main/java/effect/Brightness.java
+++ src/main/java/effect/Brightness.java
@@ -9,6 +9,7 @@ public class Brightness implements Effectible {
     private int increasingFactor;
     private int maxColorValue =255;
     private int minColorValue =0;
+
     public Brightness(String increasingFactor)
     {
             this.increasingFactor=Integer.parseInt(increasingFactor);
@@ -19,41 +20,36 @@ public class Brightness implements Effectible {
 
         int width = sourceImage.getWidth();
         int height = sourceImage.getHeight();
-        int red=0,green=0,blue=0;
+        int red, green, blue;
         BufferedImage outputImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
         for (int widthCounter = 0; widthCounter < width; widthCounter++) {
             for (int heightCounter = 0; heightCounter < height; heightCounter++) {
                 Color color = new Color(sourceImage.getRGB(widthCounter, heightCounter));
 
-
                 red = color.getRed() + increasingFactor;
                 green = color.getGreen() + increasingFactor;
                 blue = color.getBlue() + increasingFactor;
 
-                if (red >= maxColorValue +1) {
-                    red = maxColorValue;
-                } else if (red < minColorValue) {
-                    red = minColorValue;
-                }
+                red = checkBoundaryValue(red);
+                green = checkBoundaryValue(green);
+                blue = checkBoundaryValue(blue);
 
-                if (green >= maxColorValue +1) {
-                    green = maxColorValue;
-                } else if (green < minColorValue) {
-                    green = minColorValue;
-                }
-
-                if (blue >= maxColorValue +1) {
-                    blue = maxColorValue;
-                } else if (blue < minColorValue) {
-                    blue = minColorValue;
-                }
                 outputImage.setRGB(widthCounter, heightCounter, new Color(red, green, blue).getRGB());
 
             }
         }
 
         return outputImage;
+    }
+
+    public int checkBoundaryValue(int value){
+        if (value > maxColorValue ) {
+            value = maxColorValue;
+        } else if (value < minColorValue) {
+            value = minColorValue;
+        }
+        return value;
     }
 }
 
