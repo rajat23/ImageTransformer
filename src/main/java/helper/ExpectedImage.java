64455590f01d22763efd9689a8f74205ba6afd95
Rajat <rajatc@thoughtworks.com>
2015-03-24 11:13:30 +0530

    ExpectedColor class refactored

--- src/main/java/helper/ExpectedImage.java
+++ src/main/java/helper/ExpectedImage.java
@@ -6,9 +6,9 @@ import java.awt.image.BufferedImage;
 public class ExpectedImage {
     public boolean hasExpectedColor(BufferedImage image,Color expectedColor){
 
-        for(int y = 0; y < image.getHeight(); y++) {
-            for (int x = 0; x < image.getWidth(); x++) {
-                Color color=new Color(image.getRGB(x,y));
+        for(int heightCounter = 0; heightCounter < image.getHeight(); heightCounter++) {
+            for (int widthCounter = 0; widthCounter < image.getWidth(); widthCounter++) {
+                Color color=new Color(image.getRGB(widthCounter,heightCounter));
                 if(!color.equals(expectedColor))
                     return false;
             }
